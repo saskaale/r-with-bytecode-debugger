65f0745ac4d3293f51006e5f5cfebb490531755b
ales_saska <mail@alessaska.cz>
2018-05-03 02:07:10 -0500

    simple_bc example without output

--- examples/r_disassembly/simple_bc.R
+++ examples/r_disassembly/simple_bc.R
@@ -1,6 +1,15 @@
 library(compiler)
 library(bctools)
-f<-function(a) a+1
+f<-function(x) {
+    y <- x*2
+    while(x < y)
+	x <- x+1
+
+    if(x %% 2 == 0)
+	x
+    else
+	-x
+}
 compiled<-cmpfun(f)
 
 disassemble(compiled)
